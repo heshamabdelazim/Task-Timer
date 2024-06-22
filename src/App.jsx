9bf0d8efb136bf86e2710274475a8e597e967274
hesham <heshamabdelazim3@gmail.com>
2024-06-22 08:54:38 +0300

    adding features of checked and timers with some UI

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,62 @@ import Clock from "./components/analog-clock/clock";
 function App() {
   // ========useRef
   let inputText = useRef(),
-    inputTextDom = useRef();
+    inputTextDom = useRef(),
+    submitDom = useRef();
+
   // ===========useEffect
   useEffect(() => {
-    // reseting input and value
-    inputTextDom.current.value = "";
-    inputText.current = "";
+    resetting();
+    inputTextDom.current.addEventListener("input", buttonValidation);
   });
+
   // ========= useState
-  const [task, setTask] = useState([
-    // { task: "Making coffee", done: true },
-    // { task: "let's run", done: false },
-    // { task: "going to gym", done: true },
-  ]);
+  let [tasks, setTasks] = useState([]),
+    [showTime, setShowTime] = useState(null);
+
+  // ============function
+  function resetting() {
+    inputTextDom.current.value = "";
+    inputTextDom.current.focus();
+    inputText.current = "";
+    submitDom.current.classList.add("forbidden");
+  }
+
+  // ============function
+  const buttonValidation = () => {
+    !inputTextDom.current.value
+      ? submitDom.current.classList.add("forbidden")
+      : submitDom.current.classList.remove("forbidden");
+  };
 
   // ============function
   function addingTask(e) {
     e.preventDefault();
     inputText.current.length !== 0 &&
-      setTask([
-        ...task,
+      setTasks([
+        ...tasks,
         {
           task: inputText.current,
-          done: false,
+          startTime: null,
         },
       ]);
-    inputTextDom.current.value = "";
   }
-  function test() {
-    // for(){}
-  }
+  // ============function
+  const checked = (taskObj) => {
+    return tasks.filter((taskO) => {
+      return taskO.task !== taskObj.task;
+    });
+  };
+
+  // ============function
+
   return (
     <div className="to-do">
       <Container>
         <div className="parent position-relative">
-          <h1 className="text-center mb-5">To Do</h1>
+          <h1 className="m-0">Task Timer</h1>
+          <span className="m-0">Wasting your life? </span>
+          <span className="text-warning mb-5 mt-2  ms-3">watch your time.</span>
           <form className="mb-5">
             <input
               type="text"
@@ -55,31 +76,47 @@ function App() {
             <input
               type="submit"
               value="add"
+              className="forbidden"
               onClick={(e) => {
                 addingTask(e);
               }}
+              ref={submitDom}
             />
           </form>
           <ul className="list">
-            {task.length !== 0 ? (
-              task.map((taskObj, ind) => {
+            {tasks.length !== 0 ? (
+              tasks.map((taskObj, ind) => {
                 return (
                   <li key={ind} className="d-flex justify-content-between">
                     <span>{taskObj.task}</span>
                     <div className="controls d-flex align-items-center gap-4">
-                      <span className="icon-stopwatch" title="Set Time" />
-                      <span className="icon-checkmark" />
-                      <span className="icon-moon" />
+                      <span
+                        className="icon-stopwatch"
+                        title="Set Time"
+                        onClick={() => {
+                          setShowTime(taskObj.task);
+                        }}
+                      />
+                      <span
+                        className="icon-checkmark"
+                        onClick={() => setTasks(checked(taskObj))}
+                      />
                     </div>
                   </li>
                 );
               })
             ) : (
-              <h2>Nothing to show</h2>
+              <h2 style={{ color: "var(--color5)" }}>Nothing to show</h2>
             )}
           </ul>
-
           <Clock />
+          {showTime && (
+            <div className="timer-background ">
+              <section className="timer-dom">This is {showTime}</section>
+            </div>
+          )}
+          {/* when the uesr click here */}
+          {/* clicked */}
         </div>
       </Container>
     </div>
--- src/components/analog-clock/clock.jsx
+++ src/components/analog-clock/clock.jsx
@@ -1,27 +1,33 @@
-import { useRef } from "react";
+import { useEffect, useState } from "react";
 import "./clock.css";
 
 const Clock = () => {
-  const jsxArray = useRef([]);
-  for (let i = 1; i <= 12; i++) {
-    jsxArray.current.push(
-      <span key={i} style={{ transform: `rotate(${i * 30}deg)` }}>
-        <b
-          style={{
-            transform: `rotate(-${i * 30}deg)`,
-            display: "inline-block", //why this tag <b>? because you need to rotate the number itself
-          }}
-        >
-          {i}
-        </b>
-      </span>
-    );
-  }
+  const jsxArray = [];
+
+  // ========useState
+  let [hours, setHours] = useState();
+  // ========useEffect
+  useEffect(() => {
+    let jsxArray = [];
+    for (let i = 1; i <= 12; i++) {
+      jsxArray.push(
+        <span style={{ transform: `rotate(${i * 30}deg)` }}>
+          <b
+            style={{
+              transform: `rotate(-${i * 30}deg)`,
+              display: "inline-block", //why this tag <b>? because you need to rotate the number itself
+            }}
+          >
+            {i}
+          </b>
+        </span>
+      );
+      i == 12 && setHours(jsxArray);
+    }
+  }, []);
+  // why we use Effect ? because the loop alawys work if any renderes so we stop it
   // Now we got array includes 12 span
-  console.log(jsxArray.current);
-  return (
-    <section className="clock position-absolute">{jsxArray.current}</section>
-  );
+  return <section className="clock position-absolute">{hours}</section>;
 };
 
 export default Clock;
